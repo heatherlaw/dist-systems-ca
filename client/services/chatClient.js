23aab3f00eaca78bf3c3045073eccc217f0ee7c2
Heather Law <heatherlaw94@gmail.com>
2025-04-28 21:02:17 +0100

    chat validation function added

--- client/services/chatClient.js
+++ client/services/chatClient.js
@@ -13,47 +13,77 @@ var chat_proto = grpc.loadPackageDefinition(chatPackageDefinition).chat
 //connecting to server
 var client = new chat_proto.ChatBot('localhost:40000', grpc.credentials.createInsecure());
 
-//getting user's name
-var name = readlineSync.question("What is your name?");
-var call = client.chatService();
-
-call.on('data', function(resp) {
-    console.log(`From ${resp.name} : ${resp.message}`)
-});
-
-call.on('end', function() {
-
-});
-
-call.on("error", function(e) {
-    console.log ("Cannot connect to the chat server")
-});
-
-call.write ({
-    name: name,
-    message: name + " has joined the chat"
-})
-
 //creating readline interface
-var rl = readline.createInterface({
+const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-rl.on("line", function(message) {
-    if(message.toLowerCase() === "q") {
-        call.write({
-            name: name,       
-            message: name + " has left the chat"            
-        });
-        call.end();
-        rl.close();
-    }
+//getting user's name
+var name ="";
 
-    else {
-        call.write({
-            message: message,
-            name: name
-        });
-    }
-});
+function namePrompt() {
+    rl.question('What is your name? ', (userInput) => {
+        //if name isn't valid
+        if(!userInput || userInput.trim() ==="") {
+            console.log("This name is not valid. Please try again. ");
+            return namePrompt(); //ask for the name again
+        }
+
+        name = userInput.trim();
+        startChat();
+    });
+}
+
+//starting chat
+function startChat () {
+    var call = client.chatService((err, response) => {
+        if (err) {
+            console.error("An error occurred: ", err.message);
+        }
+        else {
+            console.log("Chat session ended: ", response.message);
+        }
+        rl.close();
+    });
+
+    call.on('data', function (resp) {
+        console.log(`From ${resp.name} : ${resp.message}`)
+    });
+    
+    call.on('end', function() {
+        console.log("Chat session ended. Thank you!");
+        rl.close();
+    });
+    
+    call.on("error", function(e) {
+        console.log ("Cannot connect to the chat server");
+        rl.close();
+    });
+
+    call.write ({
+        name: name,
+        message: name + " has joined the chat"
+    })
+
+    rl.on("line", function(message) {
+        if(message.toLowerCase() === "q") {
+            call.write({
+                name: name,       
+                message: name + " has left the chat"            
+            });
+            call.end();
+            rl.close();
+        }
+    
+        else {
+            call.write({
+                message: message,
+                name: name
+            });
+        }
+    });
+}
+
+//starting chat session
+namePrompt();
